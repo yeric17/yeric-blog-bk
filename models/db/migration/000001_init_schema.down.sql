d5aedbd3c936fa32e9b9fe730aca15d65a0c1598
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-01-17 00:00:15 -0500

    change database model and start creating migrate workflow

---
+++ models/db/migration/000001_init_schema.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS "email_confirms";
+DROP TABLE IF EXISTS "contacts";
+DROP TABLE IF EXISTS "parent_child_comments";
+DROP TABLE IF EXISTS "comments";
+DROP TABLE IF EXISTS "tags_posts";
+DROP TABLE IF EXISTS "tags";
+DROP TABLE IF EXISTS "posts";
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "roles";
