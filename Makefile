453d41d5e9f6ee7b5f13ed321a9ab258dbc5082b
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-01-17 21:10:22 -0500

    change env variables

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 postgres:
-	docker run --name postgres14_1 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=cirey.anaid.2107 -d postgres:14.1-alpine3.15
+	docker run --name postgres14_1 -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=uT4i%1E8GvS8 -d postgres:14.1-alpine3.15
 
 createdb:
 	docker exec -it postgres14_1 createdb --username=root --owner=root yeric-blog
@@ -8,9 +8,9 @@ dropdb:
 	docker exec -it postgres14_1 dropdb yeric-blog
 
 migrateup:
-	migrate -path models/db/migration -database "postgresql://root:cirey.anaid.2107@localhost:5433/yeric-blog?sslmode=disable" -verbose up
+	migrate -path models/db/migration -database "postgresql://root:uT4i%1E8GvS8@localhost:5433/yeric-blog?sslmode=disable" -verbose up
 
 migratedown:
-	migrate -path models/db/migration -database "postgresql://root:cirey.anaid.2107@localhost:5433/yeric-blog?sslmode=disable" -verbose down
+	migrate -path models/db/migration -database "postgresql://root:uT4i%1E8GvS8@localhost:5433/yeric-blog?sslmode=disable" -verbose down
 
 .PHONY: postgres createdb dropdb migrateup migratedown
--- config/config.go
+++ config/config.go
@@ -9,35 +9,54 @@ import (
 )
 
 var (
-	DB_USER, DB_PASS, DB_NAME, DB_HOST, DB_PORT, DB_DRIVER, APP_PORT, ConnectionString string
-	Mail                                                                               = MailStruct{}
+	DB_USER, DB_PASS, DB_NAME, DB_HOST, DB_PORT, DB_DRIVER, APP_PORT, ConnectionString, MODE string
+	Mail                                                                                     = MailStruct{}
 )
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		fmt.Println("Error loading .env file")
+		MODE = "dev"
+	} else {
+		fmt.Println("loaded env")
+		MODE = os.Getenv("MODE")
 	}
 
-	DB_USER = os.Getenv("DB_USER")
-	DB_PASS = os.Getenv("DB_PASS")
-	DB_NAME = os.Getenv("DB_NAME")
-	DB_HOST = os.Getenv("DB_HOST")
-	DB_PORT = os.Getenv("DB_PORT")
-	DB_DRIVER = os.Getenv("DB_DRIVER")
-	APP_PORT = os.Getenv("APP_PORT")
+	if MODE == "dev" {
+		fmt.Println("Running in development mode")
+		DB_USER = "root"
+		DB_PASS = "uT4i%1E8GvS8"
+		DB_NAME = "yeric-blog"
+		DB_HOST = "localhost"
+		DB_PORT = "5433"
+		DB_DRIVER = "postgres"
+		APP_PORT = "7070"
+		Mail.Host = "smtp.gmail.com"
+		Mail.Port = 587
+		Mail.Username = "mail.manager.cf2345181@gmail.com"
+		Mail.Password = "cF6XN$ozi0b3"
+	} else {
+		fmt.Println("Running in production mode")
+		DB_USER = os.Getenv("DB_USER")
+		DB_PASS = os.Getenv("DB_PASS")
+		DB_NAME = os.Getenv("DB_NAME")
+		DB_HOST = os.Getenv("DB_HOST")
+		DB_PORT = os.Getenv("DB_PORT")
+		DB_DRIVER = os.Getenv("DB_DRIVER")
+		APP_PORT = os.Getenv("APP_PORT")
+		Mail.Host = os.Getenv("MAIL_HOST")
+		Mail.Port, err = strconv.Atoi(os.Getenv("MAIL_PORT"))
 
-	Mail.Host = os.Getenv("MAIL_HOST")
-	Mail.Port, err = strconv.Atoi(os.Getenv("MAIL_PORT"))
+		if err != nil {
+			fmt.Println("MAIL_PORT is not a number")
+			os.Exit(1)
+		}
 
-	if err != nil {
-		fmt.Println("MAIL_PORT is not a number")
-		os.Exit(1)
+		Mail.Username = os.Getenv("MAIL_USER")
+		Mail.Password = os.Getenv("MAIL_PASS")
+		Mail.From = os.Getenv("MAIL_USER")
 	}
-
-	Mail.Username = os.Getenv("MAIL_USER")
-	Mail.Password = os.Getenv("MAIL_PASS")
-	Mail.From = os.Getenv("MAIL_USER")
 
 	ConnectionString = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 }
