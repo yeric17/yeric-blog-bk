08e8a76fe72b195cdfe786dc0d188bfe5e020662
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2022-01-28 00:50:46 -0500

    test with allow all origins

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	//cors policy gin config
 	router.SetTrustedProxies([]string{"http://localhost:3000", "https://yeric-blog-web.herokuapp.com"})
 	router.Use(corsgin.New(corsgin.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://yeric-blog-web.herokuapp.com"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
